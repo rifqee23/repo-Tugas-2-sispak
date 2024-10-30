f144f7c4fdcfcd08fe5ac12f35f4be07bd498792
rifqee23 <rifqifebrianto746@gmail.com>
2024-10-30 10:14:57 +0700

    feat: implement routing

---
+++ src/Pages/SupplierPages.jsx
@@ -0,0 +1,11 @@
+import { DefaultTable as Table } from "@/components/ui/Table.jsx";
+
+const SupplierPages = () => {
+  return (
+    <div>
+      <Table />
+    </div>
+  );
+};
+
+export default SupplierPages;
