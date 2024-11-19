aeb2339d97158ce329d9e92d88fb5b4ad27161a0
rifqee23 <127317331+rifqee23@users.noreply.github.com>
2024-11-19 14:51:53 +0700

    feat: integration login with api (#11)

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import useAuthStore from "@/utils/authStore";
 import { useState } from "react";
+import axios from "axios";
+import Cookies from "js-cookie";
 
 const head = [
   { label: "Username", type: "text", name: "username" },
@@ -10,21 +12,41 @@ const head = [
 ];
 const LoginPage = () => {
   const navigate = useNavigate();
-
   const [inputValues, setInputValues] = useState({});
-
   const loginUser = useAuthStore((state) => state.loginUser);
+  const [error, setError] = useState("");
 
   const handleChange = (event, fieldName) => {
     setInputValues({ ...inputValues, [fieldName]: event.target.value });
   };
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    const token = "your_access_token_here";
-    loginUser(token);
-    navigate("/");
+    const { username, password } = inputValues;
+
+    try {
+      // Mengirim request ke API login
+      const response = await axios.post(
+        "http://localhost:3000/api/auth/login",
+        {
+          username,
+          password,
+        }
+      );
+
+      if (response.data.message === "Login successfully") {
+        const token = response.data.data.token;
+        Cookies.set("access_token", token, { expires: 1 });
+        loginUser(token);
+        navigate("/");
+      } else {
+        setError("Login gagal. Cek kembali username atau password.");
+      }
+    } catch (err) {
+      setError(err.message);
+    }
   };
+
   return (
     <>
       <div className="flex justify-center items-center h-screen bg-indigo-900">
@@ -45,6 +67,7 @@ const LoginPage = () => {
           <Link className="text-white ml-6 hover:underline-offset-1 hover:underline">
             Register Account?
           </Link>
+          {error && <p className="text-red-500 mt-2">{error}</p>}
         </div>
       </div>
     </>
--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -2,8 +2,31 @@ import { Outlet } from "react-router-dom";
 import Navigation from "@/components/Navigation.jsx";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { CgProfile } from "react-icons/cg";
+import { useState, useEffect } from "react";
+import { jwtDecode } from "jwt-decode";
+
+import Cookies from "js-cookie";
 
 const MainLayout = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const fetchUserFromToken = () => {
+      try {
+        const token = Cookies.get("access_token");
+        if (token) {
+          const decoded = jwtDecode(token);
+          setUser(decoded);
+        } else {
+          console.log("Token tidak ditemukan");
+        }
+      } catch (error) {
+        console.error("Gagal mendekode token:", error.message);
+      }
+    };
+
+    fetchUserFromToken();
+  }, []);
   return (
     <div className="w-full lg:flex">
       <Navigation />
@@ -12,7 +35,8 @@ const MainLayout = () => {
         <div className="hidden fixed right-0 top-0 left-0 lg:flex items-center justify-between bg-blue-200 px-8 py-4 lg:ml-80 z-50">
           <RxHamburgerMenu />
           <div className="flex items-center gap-x-4">
-            <p>Hi, Danira</p>
+            <p>Hi, {user?.username || "User"}!</p>{" "}
+            {/* Tampilkan nama pengguna */}
             <CgProfile size={24} />
           </div>
         </div>
