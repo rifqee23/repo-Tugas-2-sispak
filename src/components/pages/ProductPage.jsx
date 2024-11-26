43812c17657afa2d1b5fdca0f088a0b12c2fdcdb
rifqee23 <rifqifebrianto746@gmail.com>
2024-11-26 18:58:39 +0700

    feat(feature): integration api update in data product

--- src/components/pages/ProductPage.jsx
+++ src/components/pages/ProductPage.jsx
@@ -4,7 +4,7 @@ import TableProduct from "../organisms/TableProduct";
 const ProductPage = () => {
   return (
     <div className="flex h-screen items-center justify-center bg-blue-gray-800">
-      <div>
+      <div className="w-full max-w-6xl p-4">
         <TableProduct />
       </div>
     </div>
